39f252f385dc1879eba82d496c348ca80602fdfc
Al Ekram Hossain Abir <abir@orangetoolz.com>
2021-07-17 21:54:15 +0600

    Added by Abir

--- app/Contracts/Services/FacebookMarketingContract.php
+++ app/Contracts/Services/FacebookMarketingContract.php
@@ -5,7 +5,30 @@ namespace App\Contracts\Services;
 
 interface FacebookMarketingContract
 {
-    public function testApi();
+    //public function testApi();
+    public function AdAccountAPI($accessToken, $actAdAccount);
+
+    public function getPageListAPI($accessToken);
+
+    public function getPixelListAPI($accessToken, $businessId);
+
+    public function getInstagramListAPI();
+
+    public function getAdAccountListAPI();
+
+    public function InvitePeopleAPI($accessToken, $businessId, $email, $role);
+
+    public function GrantAccesstoAssetsforAnotherBusinessManagerAPI();
+
+    public function getSystemUserAPI();
+
+    public function createBusinessManagerAPI();
+
+    public function clientAdAccountAPI();
+
+    public function claimClientPageAPI();
+
+    public function claimAdAccountAPI();
 }
 
 
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,16 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/init-testing', 'FacebookMarketing\FacebookMarketingController@initTesting')->name('home');
+Route::get('/adAccount', 'FacebookMarketing\FacebookMarketingController@AdAccount')->name('home');
+Route::get('/pageList', 'FacebookMarketing\FacebookMarketingController@getPageList')->name('home');
+Route::get('/pixelList', 'FacebookMarketing\FacebookMarketingController@getPixelList')->name('home');
+Route::get('/igList', 'FacebookMarketing\FacebookMarketingController@getInstagramList')->name('home');
+Route::get('/invitePeople', 'FacebookMarketing\FacebookMarketingController@InvitePeople')->name('home');
+Route::get('/grantAccesstoAssets', 'FacebookMarketing\FacebookMarketingController@GrantAccesstoAssetsforAnotherBusinessManager')->name('home');
+Route::get('/getSystemUser', 'FacebookMarketing\FacebookMarketingController@getSystemUser')->name('home');
+Route::get('/createBusinessManager', 'FacebookMarketing\FacebookMarketingController@createBusinessManager')->name('home');
+Route::get('/clientAdAccount', 'FacebookMarketing\FacebookMarketingController@clientAdAccount')->name('home');
+Route::get('/claimClientPage', 'FacebookMarketing\FacebookMarketingController@claimClientPage')->name('home');
+Route::get('/claimAdAccount', 'FacebookMarketing\FacebookMarketingController@claimAdAccount')->name('home');
+
+
